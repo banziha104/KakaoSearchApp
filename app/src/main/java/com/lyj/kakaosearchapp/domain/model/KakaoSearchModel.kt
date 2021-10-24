aab7450048b4c6aeb565a832ccd59d1c6281b876
iyeongjun <they6687@naver.com>
2021-10-24 22:26:01 +0900

    Refactor : Domain Layer Model 및 UseCase 수정
    - UseCase 사용시 정렬되도록 수정
    - List 에서 사용하는 KakaoSearchListModel 수정
    - KakaoSearchModel 에 siteUrl 추가

--- app/src/main/java/com/lyj/kakaosearchapp/domain/model/KakaoSearchModel.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/domain/model/KakaoSearchModel.kt
@@ -2,12 +2,18 @@ package com.lyj.kakaosearchapp.domain.model
 
 import java.util.Date
 
-interface KakaoSearchModel {
+sealed interface KakaoSearchModel {
     val thumbnail : String?
     val date : Date?
     val epochTimes : Long?
+    val siteUrl : String?
 }
 
 interface KakaoVClipModel : KakaoSearchModel
 
 interface KakaoImageModel : KakaoSearchModel
+
+data class KakaoSearchListModel(
+    private val kakaoSearchModel: KakaoSearchModel,
+    var isStored : Boolean = false
+) : KakaoSearchModel by kakaoSearchModel
--- app/src/main/java/com/lyj/kakaosearchapp/domain/usecase/RequestKakaoSearchResultUseCase.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/domain/usecase/RequestKakaoSearchResultUseCase.kt
@@ -5,6 +5,8 @@ import com.lyj.kakaosearchapp.domain.model.KakaoSearchModel
 import com.lyj.kakaosearchapp.domain.repository.KakaoApiRepository
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,16 +14,15 @@ import javax.inject.Singleton
 class RequestKakaoSearchResultUseCase @Inject constructor(
     private val repository: KakaoApiRepository
 ) {
-    fun execute(searchKeyword : String) : Single<List<KakaoSearchModel>> =
-        Single.zip(
-            repository.requestImageSearchApi(searchKeyword),
-            repository.requestVClipSeachApi(searchKeyword)
-        ){ images, vClips ->
-            images + vClips
+    fun execute(searchKeyword: String): Single<List<KakaoSearchModel>> = Single.zip(
+        repository.requestImageSearchApi(searchKeyword),
+        repository.requestVClipSeachApi(searchKeyword)
+    ) { images, vClips ->
+        images + vClips
+    }
+        .subscribeOn(Schedulers.io())
+        .map { list ->
+            list.sortedBy { it.epochTimes }
         }
-            .subscribeOn(Schedulers.io())
-            .map { list ->
-                list.sortedBy { it.epochTimes }
-            }
 
 }
