1c89ded065f83f2700fe7b9ec80b1731c170b811
iyeongjun <they6687@naver.com>
2021-10-24 22:37:13 +0900

    Feat : ThumbnailAdapter 구현
    - DiffUtils 를 통해 이전 데이터와 현재 데이터간 차이를 찾고 갱신
    - ListData를 옵저버 형태로 받고, 변경을 관찰하고 이를 DiffUtils 을 통해 갱신

--- app/src/main/java/com/lyj/kakaosearchapp/presentation/adapter/recycler/ThumbnailAdapter.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/adapter/recycler/ThumbnailAdapter.kt
@@ -1,27 +1,104 @@
 package com.lyj.kakaosearchapp.presentation.adapter.recycler
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jakewharton.rxbinding4.view.clicks
 import com.lyj.kakaosearchapp.R
+import com.lyj.kakaosearchapp.common.extension.lang.isNotNullAndTrue
+import com.lyj.kakaosearchapp.databinding.ItemThumbnailBinding
+import com.lyj.kakaosearchapp.domain.model.KakaoSearchListModel
+import com.lyj.kakaosearchapp.domain.model.KakaoSearchModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
-class ThumbnailAdapter : RecyclerView.Adapter<ThumbnailAdapter.ThumbnailViewHolder>(){
+class ThumbnailAdapter(
+    private val dataObserver: Flowable<List<KakaoSearchListModel>>,
+    private val onClicked: (KakaoSearchModel) -> Unit
+) : RecyclerView.Adapter<ThumbnailAdapter.ThumbnailViewHolder>() {
+
+    private var latestData: List<KakaoSearchListModel> = listOf()
+
+    init {
+        dataObserver
+            .observeOn(Schedulers.computation())
+            .map { newItems ->
+                val diffUtils = KakaoModelDiffUtils(latestData, newItems)
+                DiffUtil.calculateDiff(diffUtils) to newItems
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { (diffResult, newItems) ->
+                diffResult.dispatchUpdatesTo(this)
+                latestData = newItems
+            }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThumbnailViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_thumbnail,parent,false)
-        return ThumbnailViewHolder(view)
+        val binding =
+            ItemThumbnailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ThumbnailViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = latestData.size
 
     override fun onBindViewHolder(holder: ThumbnailViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        holder.bind(latestData[position])
     }
 
-    inner class ThumbnailViewHolder(view : View) : RecyclerView.ViewHolder(view){
+    inner class ThumbnailViewHolder(private val binding: ItemThumbnailBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            binding
+                .thumbnailItemImgContents
+                .clicks()
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe({
+                    onClicked(latestData[bindingAdapterPosition])
+                }, {
+                    it.printStackTrace()
+                })
+        }
+
+        fun bind(item: KakaoSearchListModel) {
+            Glide
+                .with(itemView)
+                .let { requestManager ->
+
+                    if (binding.thumbnailItemImgContents.tag != item.thumbnail) {
+                        requestManager
+                            .load(item.thumbnail)
+                            .into(binding.thumbnailItemImgContents.apply {
+                                tag = item.thumbnail
+                            })
+                    }
+                    requestManager
+                        .load(if (item.isStored.isNotNullAndTrue()) R.drawable.ic_check else R.drawable.ic_uncheck)
+                        .into(binding.thumbnailItemImgIsStored)
+                }
+
+        }
+    }
+}
+
+class KakaoModelDiffUtils(
+    private val oldItems: List<KakaoSearchListModel>,
+    private val newItems: List<KakaoSearchListModel>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldItems.size
+
+    override fun getNewListSize(): Int = newItems.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldItems[oldItemPosition].siteUrl == newItems[newItemPosition].siteUrl
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItems[oldItemPosition] == newItems[newItemPosition]
     }
 }
