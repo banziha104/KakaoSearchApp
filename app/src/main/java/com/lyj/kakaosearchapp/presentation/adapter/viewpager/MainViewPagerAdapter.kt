a8d9df966bb35c33adb3e40a37a8dea90df69cfb
iyeongjun <they6687@naver.com>
2021-10-24 22:34:59 +0900

    Feat : MainViewPagerAdapter 구현

---
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/adapter/viewpager/MainViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.lyj.kakaosearchapp.presentation.adapter.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.lyj.kakaosearchapp.presentation.activity.MainTabsType
+
+class MainViewPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager,lifecycle) {
+    private val tabTypes: Array<MainTabsType> = MainTabsType.values()
+
+    override fun getItemCount(): Int = tabTypes.size
+
+    override fun createFragment(position: Int): Fragment = tabTypes[position].fragment
+}
