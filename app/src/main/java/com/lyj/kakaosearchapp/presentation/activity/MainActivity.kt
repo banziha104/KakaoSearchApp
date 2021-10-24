38d8c3e37fcf6e0ab3015c0ca64686862be9bea5
iyeongjun <they6687@naver.com>
2021-10-24 22:34:34 +0900

    Feat : MainActivity, MainViewModel 구현
    - MainActivity 에서 click 관련 event 처리
    - MainViewModel 에서 저장된 데이터 관리
    - MainViewModel 에서 하위 Fragment에서 데이터를 저장하거나 삭제할 수 있도록 insertOrDeleteIfExists 메소드 구현

--- app/src/main/java/com/lyj/kakaosearchapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/activity/MainActivity.kt
@@ -2,11 +2,85 @@ package com.lyj.kakaosearchapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
+import com.jakewharton.rxbinding4.view.clicks
 import com.lyj.kakaosearchapp.R
+import com.lyj.kakaosearchapp.common.extension.android.longToast
+import com.lyj.kakaosearchapp.common.extension.android.searchButtonActionObserver
+import com.lyj.kakaosearchapp.common.extension.lang.disposeByOnDestory
+import com.lyj.kakaosearchapp.common.rx.RxLifecycleController
+import com.lyj.kakaosearchapp.common.rx.RxLifecycleObserver
+import com.lyj.kakaosearchapp.databinding.ActivityMainBinding
+import com.lyj.kakaosearchapp.presentation.adapter.viewpager.MainViewPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.kotlin.merge
+import java.lang.NullPointerException
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), OnStoredDataControlErrorHandler, RxLifecycleController {
+
+    override val rxLifecycleObserver: RxLifecycleObserver = RxLifecycleObserver(this)
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initView()
+        observeRxSource()
     }
+
+    private fun initView() {
+        initPagerAndTablayout()
+    }
+
+    private fun observeRxSource() {
+        observeClickEvent()
+    }
+
+    private fun observeClickEvent() {
+        listOf(
+            binding
+                .mainBtnSearch
+                .clicks(),
+            binding
+                .mainInputEditText
+                .searchButtonActionObserver()
+        )
+            .merge()
+            .disposeByOnDestory(this)
+            .throttleFirst(500, TimeUnit.MILLISECONDS)
+            .subscribe({
+                val text = binding.mainInputEditText.text?.toString()
+                if (text != null) {
+                    viewModel.latestSearchKeyword.value = text
+                }
+            }, {
+                it.printStackTrace()
+            })
+    }
+
+    private fun initPagerAndTablayout() {
+        binding.mainViewPager.adapter = MainViewPagerAdapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(binding.mainTabLayout, binding.mainViewPager) { tab, position ->
+            val tabType = MainTabsType.indexOf(position)
+            if (tabType != null) {
+                tab.text = getString(tabType.titleRes)
+            }
+        }.attach()
+    }
+
+    override fun onError(throwable: Throwable) {
+        longToast(if (throwable is NullPointerException) R.string.exception_url_is_null else R.string.exception_undeclared)
+    }
+}
+
+interface OnStoredDataControlErrorHandler {
+    fun onError(throwable: Throwable)
 }
--- app/src/main/java/com/lyj/kakaosearchapp/presentation/activity/MainViewModel.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/activity/MainViewModel.kt
@@ -1,4 +1,75 @@
 package com.lyj.kakaosearchapp.presentation.activity
 
-class MainViewModel {
+
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.lyj.kakaosearchapp.R
+import com.lyj.kakaosearchapp.common.view.InitializedMutableLiveData
+import com.lyj.kakaosearchapp.domain.model.KakaoSearchModel
+import com.lyj.kakaosearchapp.presentation.fragment.SearchFragment
+import com.lyj.kakaosearchapp.presentation.fragment.StoreFragment
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.Exception
+import java.lang.IndexOutOfBoundsException
+import java.lang.NullPointerException
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    val storedContents: InitializedMutableLiveData<MutableMap<String, KakaoSearchModel>> by lazy {
+        InitializedMutableLiveData(mutableMapOf())
+    }
+
+    val latestSearchKeyword: InitializedMutableLiveData<String> by lazy {
+        InitializedMutableLiveData("")
+    }
+
+    @Throws(NullPointerException::class)
+    inline fun insertOrDeleteIfExists(
+        crossinline onError: (Throwable) -> Unit
+    ): (KakaoSearchModel) -> Unit = { model ->
+        val map = storedContents.value
+        try {
+            val url = model.siteUrl!!
+            val item = map[url]
+            if (item != null) {
+                map.remove(url)
+            } else {
+                map[url] = model
+            }
+            storedContents.value = map
+        } catch (e: Exception) {
+            onError(e)
+            e.printStackTrace()
+        }
+    }
 }
+
+
+enum class MainTabsType(
+    @StringRes val titleRes: Int,
+    val fragment: Fragment,
+) {
+    SEARCH(R.string.main_tab_item_search, SearchFragment.instance),
+    STORE(R.string.main_tab_item_store, StoreFragment.instance);
+
+    companion object {
+        private val tabTypes = values()
+        fun indexOf(position: Int): MainTabsType? = try {
+            if (position >= tabTypes.size) throw IndexOutOfBoundsException()
+            tabTypes[position]
+
+        } catch (e: IndexOutOfBoundsException) {
+            e.printStackTrace()
+            null
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
+
