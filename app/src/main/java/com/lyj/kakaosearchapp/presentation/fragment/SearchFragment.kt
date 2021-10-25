560208eededa51b63cc7e9f6d079a3128855b6a8
iyeongjun <they6687@naver.com>
2021-10-25 15:53:46 +0900

    Feat : SearchFragment 에 UI Event Type 및 해당 Event에 따른 분기 추가

--- app/src/main/java/com/lyj/kakaosearchapp/presentation/fragment/SearchFragment.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/fragment/SearchFragment.kt
@@ -9,17 +9,23 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.toPublisher
 import androidx.recyclerview.widget.GridLayoutManager
+import com.lyj.kakaosearchapp.common.extension.android.observeEndScrollEvents
+import com.lyj.kakaosearchapp.common.extension.android.refreshObserver
 import com.lyj.kakaosearchapp.common.extension.lang.disposeByOnDestory
 import com.lyj.kakaosearchapp.common.rx.RxLifecycleController
 import com.lyj.kakaosearchapp.common.rx.RxLifecycleObserver
+import com.lyj.kakaosearchapp.data.source.remote.service.KakaoSearchApi
 import com.lyj.kakaosearchapp.databinding.SearchFragmentBinding
 import com.lyj.kakaosearchapp.domain.model.KakaoSearchListModel
 import com.lyj.kakaosearchapp.domain.model.KakaoSearchModel
 import com.lyj.kakaosearchapp.presentation.activity.MainViewModel
 import com.lyj.kakaosearchapp.presentation.activity.OnStoredDataControlErrorHandler
 import com.lyj.kakaosearchapp.presentation.adapter.recycler.ThumbnailAdapter
+import com.lyj.kakaosearchapp.presentation.adapter.recycler.ThumbnailItemEvent
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.core.BackpressureStrategy
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(), RxLifecycleController {
@@ -38,7 +44,7 @@ class SearchFragment : Fragment(), RxLifecycleController {
     private val viewModel: SearchViewModel by viewModels()
     private val activityViewModel: MainViewModel by activityViewModels()
 
-    private val searchKeywordChangeObserver: Flowable<List<KakaoSearchModel>> by lazy {
+    private val searchKeywordChangeObserver: Flowable<String> by lazy {
         viewModel
             .mapToEditTextObserverByPublisher(
                 activityViewModel
@@ -58,11 +64,50 @@ class SearchFragment : Fragment(), RxLifecycleController {
             )
     }
 
+    private val uiEventToRequestObserver: Flowable<Pair<SearchFragmentUiEventType, List<KakaoSearchModel>>> by lazy {
+        Flowable
+            .merge<SearchFragmentUiEventType>(
+                searchKeywordChangeObserver
+                    .map {
+                        viewModel.page = KakaoSearchApi.DEFAULT_PAGE
+                        SearchFragmentUiEventType.Search(it)
+                    },
+                binding.searchRecyclerView.observeEndScrollEvents(this)
+                    .map { 
+                        SearchFragmentUiEventType.EndScroll(++viewModel.page)
+                    }
+                    .toFlowable(BackpressureStrategy.LATEST),
+                binding.serachSwipeRefreshLayout.refreshObserver()
+                    .map {
+                        viewModel.page = KakaoSearchApi.DEFAULT_PAGE
+                        SearchFragmentUiEventType.Refresh
+                    }
+                    .toFlowable(BackpressureStrategy.LATEST)
+            )
+            .flatMapSingle { event ->
+                when (event) {
+                    is SearchFragmentUiEventType.EndScroll -> viewModel.requestKakaoSearchResultUseCase.execute(
+                        activityViewModel.latestSearchKeyword.value,
+                        event.page
+                    )
+                    SearchFragmentUiEventType.Refresh -> viewModel.requestKakaoSearchResultUseCase.execute(
+                        activityViewModel.latestSearchKeyword.value
+                    )
+                        .doOnSubscribe { binding.serachSwipeRefreshLayout.isRefreshing = false }
+                    is SearchFragmentUiEventType.Search -> viewModel.requestKakaoSearchResultUseCase.execute(
+                        event.searchKeyword
+                    )
+                }.map { event to it }
+            }
+            .disposeByOnDestory(this)
+
+    }
+
     private val searchThumbnailAdapter: ThumbnailAdapter by lazy {
         val handler = (activity as? OnStoredDataControlErrorHandler) ?: throw NotImplementedError()
         val onClicked = activityViewModel.insertOrDeleteIfExists(handler::onError)
         val dataFlow = viewModel.mapToKakaoSearchList(
-            remoteDataObserver = searchKeywordChangeObserver,
+            remoteDataObserver = uiEventToRequestObserver,
             storedDataObserver = storedDataChangeObserver,
             this
         )
@@ -91,4 +136,11 @@ class SearchFragment : Fragment(), RxLifecycleController {
                 layoutManager = GridLayoutManager(requireContext(), GRID_SPAN_COUNT)
             }
     }
+
+    sealed interface SearchFragmentUiEventType {
+        object Refresh : SearchFragmentUiEventType
+        class Search(val searchKeyword: String) : SearchFragmentUiEventType
+        class EndScroll(val page: Int) : SearchFragmentUiEventType
+    }
 }
+
--- app/src/main/java/com/lyj/kakaosearchapp/presentation/fragment/SearchViewModel.kt
+++ app/src/main/java/com/lyj/kakaosearchapp/presentation/fragment/SearchViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.toPublisher
 import com.lyj.kakaosearchapp.common.extension.lang.disposeByOnDestory
 import com.lyj.kakaosearchapp.common.rx.RxLifecycleController
+import com.lyj.kakaosearchapp.data.source.remote.service.KakaoSearchApi
 import com.lyj.kakaosearchapp.domain.model.KakaoSearchListModel
 import com.lyj.kakaosearchapp.domain.model.KakaoSearchModel
 import com.lyj.kakaosearchapp.domain.usecase.RequestKakaoSearchResultUseCase
+import com.lyj.kakaosearchapp.presentation.adapter.recycler.ThumbnailItemEvent
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Flowable
@@ -15,19 +17,18 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val requestKakaoSearchResultUseCase: RequestKakaoSearchResultUseCase
+    val requestKakaoSearchResultUseCase: RequestKakaoSearchResultUseCase
 ) : ViewModel() {
+
+    var page : Int = KakaoSearchApi.DEFAULT_PAGE
 
     fun mapToEditTextObserverByPublisher(
         publisher: Publisher<String>,
         lifecycleController: RxLifecycleController
-    ): Flowable<List<KakaoSearchModel>> =
+    ): Flowable<String> =
         Flowable
             .fromPublisher(publisher)
             .filter { it.isNotBlank() }
-            .flatMapSingle {
-                requestKakaoSearchResultUseCase.execute(it)
-            }
             .disposeByOnDestory(lifecycleController)
 
 
@@ -40,17 +41,21 @@ class SearchViewModel @Inject constructor(
             .disposeByOnDestory(lifecycleController)
 
     fun mapToKakaoSearchList(
-        remoteDataObserver : Flowable<List<KakaoSearchModel>>,
+        remoteDataObserver : Flowable<Pair<SearchFragment.SearchFragmentUiEventType,List<KakaoSearchModel>>>,
         storedDataObserver : Flowable<MutableMap<String,KakaoSearchModel>>,
         lifecycleController: RxLifecycleController
-    ): Flowable<List<KakaoSearchListModel>> =
+    ): Flowable<ThumbnailItemEvent> =
         Flowable
             .combineLatest(
                 remoteDataObserver,
                 storedDataObserver
-            ) { remoteData, storedData ->
-                remoteData.map { model ->
+            ) { (event,remoteData), storedData ->
+                val result = remoteData.map { model ->
                     KakaoSearchListModel(model, storedData[model.siteUrl] != null)
+                }
+                when(event){
+                    is SearchFragment.SearchFragmentUiEventType.EndScroll -> ThumbnailItemEvent.Add(result)
+                    else -> ThumbnailItemEvent.Refesh(result)
                 }
             }
             .disposeByOnDestory(lifecycleController)
